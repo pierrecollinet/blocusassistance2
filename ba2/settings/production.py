927e31748b791e99ff75b1f8896818727639737c
Pierre Collinet <pi.collinet@gmail.com>
2017-12-27 13:38:29 +0100

    ...

--- ba2/settings/production.py
+++ ba2/settings/production.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
+qsfqsfd
 print("je suis en production!")
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
