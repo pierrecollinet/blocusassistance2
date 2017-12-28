7207577ec64d507c8b91fbc7bfd5f1ade9f5da0d
Pierre Collinet <pi.collinet@gmail.com>
2017-12-28 11:16:34 +0100

    compress html

--- ba2/settings/production.py
+++ ba2/settings/production.py
@@ -63,6 +63,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'htmlmin.middleware.HtmlMinifyMiddleware',
 )
 
 ROOT_URLCONF = 'ba2.urls'
