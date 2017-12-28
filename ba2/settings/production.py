e50b68bfc3b156130a231ecbb8d1f9fb8fe58236
Pierre Collinet <pi.collinet@gmail.com>
2017-12-28 11:42:56 +0100

    minified html

--- ba2/settings/production.py
+++ ba2/settings/production.py
@@ -63,6 +63,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     'htmlmin.middleware.HtmlMinifyMiddleware',
     'htmlmin.middleware.MarkRequestMiddleware',
 )
